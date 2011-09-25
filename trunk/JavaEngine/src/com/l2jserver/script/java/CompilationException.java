be76000428c270592862eb8ca2d3159a81335ace
MELERIX <MELERIX@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-09-25 14:34:09 +0000

    Importing Java/Jython Engines (NOTE: This libs require updates for Java 7 also recompile).

---
+++ trunk/JavaEngine/src/com/l2jserver/script/java/CompilationException.java
@@ -0,0 +1,10 @@
+package com.l2jserver.script.java;
+
+public class CompilationException extends Exception {
+
+	public CompilationException(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
---
+++ trunk/JavaEngine/src/com/l2jserver/script/java/tests/Tests.java
@@ -0,0 +1,30 @@
+package com.l2jserver.script.java.tests;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class Tests {
+
+	public static void main(String args[]) throws ScriptException, IOException {
+		ScriptEngineManager sem = new ScriptEngineManager();
+		ScriptEngine se = sem.getEngineByExtension("java");
+		se.getContext().setAttribute("classpath", "E:/shared/workspaces/workspace_c6/EclipseJavaScriptEngine/TestCode", 100);
+		se.getContext().setAttribute("mainClass", "Test", 100);
+		se.getContext().setAttribute("javax.script.filename", "Test.java", 100);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("TestCode/Test.java")));
+		se.eval(br);
+		System.err.flush();
+		System.out.flush();
+		System.err.println("PRESS ANY KEY AFTER EDITING FILE");
+		System.in.read();
+		br = new BufferedReader(new InputStreamReader(new FileInputStream("TestCode/Test.java")));
+		se.eval(br);
+	}
+}
